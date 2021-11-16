64dd65dabd2482076c643d189d47d00d4cac6424
kwonikhyeon <kwen1028@naver.com>
2021-11-17 01:02:50 +0900

    Update contents.py

--- contents.py
+++ contents.py
@@ -11,35 +11,31 @@ class Person:
         self.hp = maxhp
         
     def normalAttack(self, target): #일반공격
-        print(self.name, '일반 공격 선택')
         if random.randint(1,10) in range(1,10): #90% 확률로 일반공격 성공
             if self.att >= target.defence: #내 공격력이 상대 방어력보다 높아야 공격가능
                 target.hp -= (self.att - target.defence)
-                print(f"일반공격 성공! 데미지 {self.att - target.defence}\n")
+                return f"일반공격 성공! 데미지 {self.att - target.defence} "
             else:
-                print("공격이 무시되었습니다.\n")
+                return "공격이 무시되었습니다. "
         else:
-            print("일반 공격을 실패했습니다.\n")
+            return "일반 공격을 실패했습니다. "
 
     def criticalAttack(self, target): #크리티컬공격
-        print(self.name, '크리티컬 공격 선택')
         if random.randint(1,100) in range(1,self.luck-11): #(luck-10)% 확률로 크리티컬 공격 성공
             if self.att*3 >= target.defence: #내 공격력이 상대 방어력보다 높아야 공격가능
                 target.hp -= (self.att*3 - target.defence)
-                print(f"크리티컬 공격 성공! 데미지 {self.att*3 - target.defence}\n")
+                return f"크리티컬 공격 성공! 데미지 {self.att*3 - target.defence} "
             else:
-                print("공격이 무시되었습니다.\n")
+                return "공격이 무시되었습니다. "
         else:
-            print("크리티컬 공격을 실패했습니다.\n")
+            return "크리티컬 공격을 실패했습니다. "
 
     def heal(self):
-        print(self.name, '회복 선택')
         self.hp += self.hp*0.25
         if self.hp >= self.maxhp:
             self.hp = self.maxhp
-            print("체력이 가득 찼습니다!")
-            return
-        print(f"체력이 {self.hp*0.25} 회복되었습니다.\n")
+            return "체력이 가득 찼습니다! "
+        return f"체력이 {int(self.hp*0.25)} 회복되었습니다. "
 
     def state(self):
         print('[', self.name, ']')
@@ -62,14 +58,14 @@ class Com(Person):
         p = random.random()
         #회복
         if p > self.pos[1]:
-            print(self.name, '회복 선택')
-            self.heal()
+            actionNum = 2
+            msg = self.heal()
         #크리티컬
         elif p > self.pos[0]:
-            print(self.name, '크리티컬 공격 선택')
-            self.criticalAttack(target)
+            actionNum = 1
+            msg = self.criticalAttack(target)
         #일반공격
         else:
-            print(self.name, '일반 공격 선택')
-            self.normalAttack(target)
-
+            actionNum = 0
+            msg = self.normalAttack(target)
+        return actionNum, msg
