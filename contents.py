f91d67f467ae950259d490f37424adffb70e8b00
kwonikhyeon <kwen1028@naver.com>
2021-11-20 02:02:46 +0900

    Update contents.py

--- contents.py
+++ contents.py
@@ -49,48 +49,60 @@ class Player(Person):
         super().__init__(name, sex, att, defence, luck, maxhp)
         self.coin = 40
         self.level = 1
-        self.skill = [1,1,1,3,3]
+        self.skill = [1,0,0,3,3]
         self.item = [0,0,0,0,0]
+        self.poisonTurn = 0
 
-    def poison(self, target, turn):
-        if self.skill[0] == 0:
-            return
-        if turn < 3:
-            return True
+    def poison(self, target, mode):# 미완성
+        if mode == 0:
+            if self.skill[0] == 0:
+                return "", False, False
+            if self.poisonTurn > 0:
+                return "이미 독에 중독되어있습니다. ", True, True
+            return "상대방이 독에 중독되었습니다. ", True, True
+        
+        percent = [0,0.05,0.07,0.1]
+        if mode == 1:
+            if self.poisonTurn == 3:
+                self.poisonTurn = 0
+                return "", False
+            target.hp -= target.maxhp*percent[self.skill[0]]
+            self.poisonTurn += 1
+            return f"독 데미지 {int(target.maxhp*percent[self.skill[0]])} ", True
 
     def def_decrease(self, target):
         if self.skill[1] == 0:
-            return
+            return "", False
         percent = [0,0.1,0.3,0.5]
         target.defence -= target.defence*percent[self.skill[1]]
-        return f"상대 방어력 {100*percent[self.skill[1]]}% 감소! "
+        return f"상대 방어력 {100*percent[self.skill[1]]}% 감소! ", True
 
     def att_decrease(self, target):
         if self.skill[2] == 0:
-            return
+            return "", False
         percent = [0,0.1,0.2,0.3]
         target.att -= target.att*percent[self.skill[2]]
-        return f"상대 공격력 {100*percent[self.skill[2]]}% 감소! "
+        return f"상대 공격력 {100*percent[self.skill[2]]}% 감소! ", True
 
     def reflect(self, target, targetDmg):
         if self.skill[3] == 0:
-            return
+            return "", False
         percent = [0,0.5,0.75,1]
         if random.random() < percent[self.skill[3]]:
             self.hp += targetDmg
             if self.hp >= self.maxhp: self.hp = self.maxhp
             dmg = targetDmg - target.defence
             target.hp -= dmg
-            return f"반사 성공! 체력 회복! 데미지 {int(dmg)} "
-        else: return "반사를 실패했습니다.. "
+            return f"반사 성공! 체력 회복! 데미지 {int(dmg)} ", True
+        else: return "반사를 실패했습니다.. ", True
 
     def absoluteAtt(self, target):
         if self.skill[4] == 0:
-            return
+            return "", False
         percent = [0,0.2,0.3,0.5]
         dmg = target.hp*percent[self.skill[4]]
         target.hp -= dmg
-        return f"방어력 무시! 데미지 {int(dmg)} "
+        return f"방어력 무시! 데미지 {int(dmg)} ", True
 
 
 class Com(Person):
